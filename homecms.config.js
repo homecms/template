e5d7ecdee0ea31e592335b0a7665fa270f62dfdc
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2022-11-16 18:31:31 +0000

    chore: initial commit

---
+++ homecms.config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @see https://github.com/homecms/homecms#readme for config options.
+ */
+
+/**
+ * @type {import('homecms').Config}
+ */
+module.exports = {
+	// Your config options go here
+};
